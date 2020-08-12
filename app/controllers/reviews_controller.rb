a93f6dc68a727ea40b32c83c4d1fba34399103ef
Erika Sinkovics <erika.sinkovics@outlook.com>
2020-08-12 14:43:53 -0400

    Configured controllers

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,18 @@
 class ReviewsController < ApplicationController
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.save
+
+    redirect_to review_path(@review)
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:review).permit(:content, :rating)
+  end
 end
